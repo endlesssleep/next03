5478a81ee396f9fe6a4d5afcda8ea9706d234ad0
akzmffls <d7703_14@d7703-14ui-imac.local>
2019-03-28 11:34:18 +0900

    Initial Commit

---
+++ 12sdf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  12sdf
+//
+//  Created by D7703_14 on 2019. 3. 28..
+//  Copyright © 2019년 Personal Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
